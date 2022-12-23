534a60ac077dfc33fd9ce5563fd45f94fe14bf50
mauricio.monte <mauricio.monte@splab.ufcg.edu.br>
2022-12-23 10:11:08 -0300

    Change URL pattern

--- src/main/java/com/petcare/backend/util/UrlConstants.java
+++ src/main/java/com/petcare/backend/util/UrlConstants.java
@@ -2,8 +2,8 @@ package com.petcare.backend.util;
 
 public class UrlConstants {
     public static final String BASE_URL = "api/v1/";
-    public static final String USER_URL = BASE_URL + "user";
-    public static final String VACCINE_URL = BASE_URL + "vaccine";
-    public static final String PET_URL = BASE_URL + "pet";
-    public static final String DOSE_URL = BASE_URL + "dose";
+    public static final String USER_URL = "users";
+    public static final String VACCINE_URL = "vaccines";
+    public static final String PET_URL = "pets";
+    public static final String DOSE_URL = "doses";
 }
