893af3870b97349ae7a40b05be43c8a2a7c141e8
mauricio.monte <mauricio.monte@splab.ufcg.edu.br>
2023-01-03 16:48:17 -0300

    Change spring boot security configuration

--- src/main/java/com/petcare/backend/PetCareBackendApplication.java
+++ src/main/java/com/petcare/backend/PetCareBackendApplication.java
@@ -2,7 +2,6 @@ package com.petcare.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,5 +13,4 @@ public class PetCareBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetCareBackendApplication.class, args);
 	}
-
 }
